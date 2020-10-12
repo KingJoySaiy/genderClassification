0ec8cc22ce0badc3454d88e608b14304b9352266
KingJoySaiy <kingjoysaiy@gmail.com>
2020-10-12 15:48:47 +0800

    logistic regression(pytorch) success

--- readFile.py
+++ readFile.py
@@ -2,58 +2,87 @@ import csv
 import numpy as np
 from matplotlib import image
 
-rootPath = 'C:\\KingJoySaiy\\workspace\\genderClassification\\jiangnan2020_Too_Simple\\'
+rootPath = 'jiangnan2020_Simple\\'
 
 # Train: [1.jpg, 18000.jpg], csv: {id -> label}
 trainCSV = rootPath + 'train.csv'
 trainImage = rootPath + 'train\\train\\'
 
 # test: [18001.jpg, 23708.jpg], csv: {id}
-testCSV = rootPath + 'test.csv'
-testImage = rootPath + 'test\\test\\'
+# testCSV = rootPath + 'test.csv'
+# testImage = rootPath + 'test\\test\\'
 
 imageW = 200
 imageH = 200
 
+# # read image numpy.ndarray(200, 200, 3)
+# def readImage(path):
+#     im = image.imread(path)
+#     # print(type(im))
+#     return im
+#
+#
+# # training data(trainSize, 200, 200, 3), label:
+# def getTrainData():
+#     # get training id & label
+#     id = []
+#     label = []
+#     reader = csv.reader(open(trainCSV, 'r'))
+#     next(reader)
+#     for row in reader:
+#         id.append(int(row[0]))
+#         label.append(int(row[1]))
+#
+#     # get matrix of training image
+#     data = np.zeros((len(id), imageH, imageW, 3))
+#     ct = 0
+#     for i in id:
+#         data[ct] = readImage(trainImage + str(i) + '.jpg')
+#         ct += 1
+#     return data, np.array(label).reshape(len(label), 1)
+#
+#
+# # testing data(testSize, 200, 200, 3)
+# def getTestData():
+#     # get testing id
+#     testId = []
+#     reader = csv.reader(open(testCSV, 'r'))
+#     next(reader)
+#     for row in reader:
+#         testId.append(int(row[0]))
+#
+#     # get matrix of testing image
+#     data = np.zeros((len(testId), imageH, imageW, 3))
+#     ct = 0
+#     for i in testId:
+#         data[ct] = readImage(testImage + str(i) + '.jpg')
+#         ct += 1
+#     return data
 
-# read image numpy.ndarray(200, 200, 3)
+SIZE = imageW * imageH * 3
+ct = 0
+
+
 def readImage(path):
-    im = image.imread(path)
-    # print(type(im))
+    global ct
+    print(ct)
+    ct += 1
+    im = image.imread(path).reshape(1, SIZE)
     return im
 
 
-# Data, Label: image(200 * 200) & label of each trainImage
 def getTrainData():
-    # get directory of training label {id -> label}
-    idLabel = dict()
+    id = []
+    label = []
     reader = csv.reader(open(trainCSV, 'r'))
     next(reader)
     for row in reader:
-        idLabel[int(row[0])] = int(row[1])
+        id.append(int(row[0]))
+        label.append(int(row[1]))
 
-    # get list of training image
-    data = np.zeros(len(idLabel), 1)
+    data = np.zeros((len(id), SIZE))
     ct = 0
-    for i in idLabel.keys():
-        data[ct, 0] = readImage(trainImage + str(i) + '.jpg')
+    for i in id:
+        data[ct] = readImage(trainImage + str(i) + '.jpg')
         ct += 1
-    return data, list(idLabel.values())
-
-
-# Data: image(200 * 200) of each testImage
-def getTestData():
-    # get list of test {Id}
-    testId = []
-    reader = csv.reader(open(testCSV, 'r'))
-    next(reader)
-    for row in reader:
-        testId.append(int(row[0]))
-
-    # get directory of training image {id -> matrix}
-    data = np.zeros(len(testId), 1)
-    ct = 0
-    for i in testId:
-        data[ct, 0] = readImage(testImage + str(i) + '.jpg')
-        ct += 1
-    return data
+    return np.hstack((data, np.array(label).reshape(len(label), 1)))
