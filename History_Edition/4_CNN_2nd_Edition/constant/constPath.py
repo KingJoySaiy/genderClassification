4019a72a085b310496d9b6e4078b8a75fa5615b6
KingJoySaiy <kingjoysaiy@gmail.com>
2020-10-16 17:00:38 +0800

    CNN 2nd edition (up to 87%)

---
+++ History_Edition/4_CNN_2nd_Edition/constant/constPath.py
@@ -0,0 +1,29 @@
+# for windows laptop
+rootPath = 'jiangnan2020_Simple\\'
+trainImage = rootPath + 'train\\train\\'
+testImage = rootPath + 'test\\test\\'
+modelPath = 'model\\CNN.pkl'
+trainBatch = 100
+predictBatch = 50
+
+'''
+# for linux server
+rootPath = 'jiangnan2020/'
+trainImage = rootPath + 'train/train/'
+testImage = rootPath + 'test/test/'
+modelPath = 'model/CNN.pkl'
+trainSize = 1000
+predictBatch = 500
+'''
+
+trainCSV = rootPath + 'train.csv'
+testCSV = rootPath + 'test.csv'
+submitCSV = rootPath + 'submit.csv'
+
+imageW = 200
+imageH = 200
+globalSeed = 233
+
+learningRate = 0.001
+initialMomentum = 0.9
+weightDecay = 1e-4
